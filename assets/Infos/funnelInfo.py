ff50f293d772ca75817981555ff6fd4c89f71435
Tianqi Yu <leatqy@outlook.com>
2025-11-09 15:35:52 +0100

    added more info for standard funnel plot

--- assets/Infos/funnelInfo.py
+++ assets/Infos/funnelInfo.py
@@ -10,7 +10,7 @@ funnel_info = html.P("Comparison-adjusted funnel plots where each point correspo
 infoFunnel = __info_modal("funnel", "Info for Comparison-adjusted Funnel Plot", funnel_info)
 
 
-funnel_info2 = html.P("Standard funnel plots display each study's effect estimate against a measure of its precision (such as sample size or standard error). In a typical funnel-shaped plot, smaller and less precise studies appear widely scattered at the bottom, while larger and more precise studies cluster toward the top with narrower spread. If the plot shows asymmetry or missing points at the bottom, it may indicate publication bias, where studies with non-significant or unfavorable results are less likely to be published."
+funnel_info2 = html.P("Standard funnel plots display each study's effect estimate against a measure of its precision (such as sample size or standard error). In a typical funnel-shaped plot, smaller and less precise studies appear widely scattered at the bottom, while larger and more precise studies cluster toward the top with narrower spread. If the plot shows asymmetry or missing points at the bottom, it may indicate publication bias, where studies with non-significant or unfavorable results are less likely to be published. \n Users can select a comparison that includes at least two studies in the network diagram to generate the plot, and at least ten studies are required to assess potential asymmetry."
                     ,className="infoModal"
                     ,style={'display':'inline-block',"text-align":'left', 'font-size': 'large'}
                     )
